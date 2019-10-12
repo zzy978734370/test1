09ff59b9691d973f5e4e551c0bd29e59e953c0b0
zhaozhenyu <978734370@qq.com>
2019-10-12 17:40:35 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    /**
+     * @author 赵振宇
+     * @date 2019/10/12 0012
+     * @time 17:31
+     * @desc
+     */
+
+    public static void main(String[] args) {
+        System.out.println("这个类用来测试直接下载的项目是否能运行");
+    }
+}
